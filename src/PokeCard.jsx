c37c8204e3db8ca5d2a4a53ed95aa12b63494e56
Angel Giron <angel.c.giron1@gmail.com>
2022-11-09 14:42:29 -0800

    removed see details btn

--- src/PokeCard.jsx
+++ src/PokeCard.jsx
@@ -28,9 +28,9 @@ export default class PokeCard extends Component {
         typeString += allTypes[i][0].toUpperCase() + allTypes[i].slice(1) + ' | ';
       } 
     }
- 
-
-    let bio = speciesData.flavor_text_entries[4].flavor_text.replace('POKéMON', 'Pokémon').replace(data.name.toUpperCase(), data.name[0].toUpperCase() + data.name.slice(1));
+    let reg = new RegExp(data.name, 'gi')
+    let bio = speciesData.flavor_text_entries[1].flavor_text.replace(/POKéMON/gi, 'Pokémon').replace(reg, data.name[0].toUpperCase() + data.name.slice(1));
+    let genus = speciesData.genera[7].genus;
 
     return (
       <div className='main-container-two'>
@@ -45,21 +45,22 @@ export default class PokeCard extends Component {
           </div>
           <button onClick={() => this.props.findPokemon(data.id + 1)} className='navigate'><i className="fa fa-thin fa-caret-right"></i></button>
         </div>
-        <button 
+        {/* <button 
           className='details-btn' onClick={this.toggleDetails}>{this.state.seeDetails ? 'Hide' : 'See'} details
-        </button>
-        {this.state.seeDetails && 
+        </button> */}
+        {/* {this.state.seeDetails &&  */}
         <div className='card-details'>
           <h4 className='card-name'>
             {data.name[0].toUpperCase() + data.name.slice(1)}
           </h4>
           <div className='card-info'>
-            {<p>{`#${data.id}`}</p>}
-            {<p>{typeString}</p>}
+            <p>{'#' + data.id}</p>
+            <p>{typeString}</p>
             {speciesData.evolves_from_species && <p>{`Evolves from ${speciesData.evolves_from_species.name[0].toUpperCase() + speciesData.evolves_from_species.name.slice(1)}`}</p>}
             {<p>{bio}</p>}
           </div>
-        </div>}
+        </div>
+        {/* } */}
       </div>
     )
   }
