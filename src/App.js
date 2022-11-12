d9fc7be8dab3a7697df424ca38436ec7eb3feea3
Angel Giron <angel.c.giron1@gmail.com>
2022-11-12 10:52:07 -0800

    added dream and official artwork

--- src/App.js
+++ src/App.js
@@ -45,14 +45,16 @@ class App extends Component {
       // if pokemon is in cache
     } else if (this.state.cache[searchData]) {
       // console.log('found in cache');
+      console.log(searchData);
       this.setState({
         inCache: this.state.cache[searchData].inCache,
         loaded: true,
         data: this.state.cache[searchData].data,
         speciesData: this.state.cache[searchData].speciesData,
-        image:
-          this.state.cache[searchData].data.sprites.other['official-artwork']
-            .front_default,
+        // image:
+        //   this.state.cache[searchData].data.sprites.other['official-artwork']
+        //     .front_default,
+        image: this.state.cache[searchData].image,
         input: '',
       });
       return;
@@ -66,14 +68,18 @@ class App extends Component {
       const speciesData = await axios.get(
         `https://pokeapi.co/api/v2/pokemon-species/${searchData}`
       );
-      // console.log('species', speciesData.data);
       this.setState({
         input: '',
         loaded: true,
         inCache: false,
         data: pokemonData.data,
-        image: pokemonData.data.sprites.other['official-artwork'].front_default,
-        // image: res.data.sprites.other.dream_world.front_default,
+        // image: pokemonData.data.sprites.other['official-artwork'].front_default,
+        image:
+          pokemonData.data.id <= 649
+            ? pokemonData.data.sprites.other.dream_world.front_default
+            : pokemonData.data.sprites.other['official-artwork'].front_default,
+        // pokemonData.data.sprites.other.dream_world.front_default,
+
         speciesData: speciesData.data,
         cache: {
           ...this.state.cache,
@@ -81,7 +87,10 @@ class App extends Component {
             ...this.state.cache[searchData],
             data: pokemonData.data,
             image:
-              pokemonData.data.sprites.other['official-artwork'].front_default,
+              pokemonData.data.id <= 649
+                ? pokemonData.data.sprites.other.dream_world.front_default
+                : pokemonData.data.sprites.other['official-artwork']
+                    .front_default,
             speciesData: speciesData.data,
             inCache: true,
           },
