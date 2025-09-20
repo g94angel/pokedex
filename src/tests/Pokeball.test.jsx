ba671254c3eac18b2ef8fbe2caff27ba47e0012a
Angel Giron <angel.giron@techngs.com>
2025-09-19 18:49:56 -0600

    removed packages

--- src/tests/Pokeball.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Pokeball from "../components/Pokeball";
-
-test("renders Pokeball component", () => {
-  const { container } = render(<Pokeball />);
-  const pokeball = container.querySelector(".pokeball");
-  expect(pokeball).toBeInTheDocument();
-});
